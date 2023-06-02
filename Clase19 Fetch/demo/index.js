42b0ef84a6bd7b555c02862fd4895e97e8317bb5
si390 <soyolaiberico@udesa.edu.ar>
2023-06-02 10:29:17 -0300

    rickandmorty

--- Clase19 Fetch/demo/index.js
+++ Clase19 Fetch/demo/index.js
@@ -25,6 +25,33 @@ fetch(url)
     })
 
 
+//Crear la funcionalidad para crear un nuevo gif
+let nuevogif = document.querySelector(".nuevogif");
+
+nuevogif.addEventListener("click", function(e){
+    e.preventDefault();
+    fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data.data.title);
+        let titulogif = document.querySelector(".titulogif");
+        if (data.data.title == "") {
+            titulogif.innerText = "Este gif no tiene titulo"
+        } else{
+            titulogif.innerText = data.data.title
+        }
+
+        let imagen = document.querySelector(".gif");
+        imagen.src = data.data.images.original.url
+
+    })
+    .catch(function (error) {
+        return console.error(error);
+    })
+})
+
 
 //Provincias Array
 let urlprovincias = "https://apis.datos.gob.ar/georef/api/provincias";
@@ -37,13 +64,18 @@ fetch(urlprovincias)
         console.log(data);
         let provinciasContainer = document.querySelector(".provincias");
         let arrayProvincias = data.provincias;
+        let provincias = ""
         for (let i = 0; i < arrayProvincias.length; i++){
-
             //cada vuelta del for tiene que armar un elemento de la lista
-           
+           //provincias += "<li>" + arrayProvincias[i].nombre +"</li>" 
+           //Otra forma provincias = "<li>" + arrayProvincias[i].nombre +"</li>" + provincias
+           provincias += `<li>${arrayProvincias[i].nombre}</li>`
         }
+
+        provinciasContainer.innerHTML = provincias;
 
     })
     .catch(function (error) {
         return console.error(error);
     })
+
---
+++ Clase19 Fetch/practica/index.js
@@ -0,0 +1,28 @@
+console.log("Practica Playground Rick and Morty");
+
+let url = "https://rickandmortyapi.com/api/character";
+
+fetch(url)
+.then(function(response) {
+    return response.json()
+})
+.then(function(data) {
+    console.log(data);
+    let seccion = document.querySelector(".characterList");
+    let arraycharacters = data.results
+    let characters = ""
+
+    for(let i = 0; i < arraycharacters.length; i++){
+        characters += `<article>
+                            <img src=${arraycharacters[i].image} alt='' />
+                            <p>Name:${arraycharacters[i].name}</p>
+                            <p>Status:${arraycharacters[i].status}</p>
+                        </article>`
+
+    }
+        seccion.innerHTML = characters;
+
+})
+.catch(function(error) {
+    console.log("Error: " + error);
+})
