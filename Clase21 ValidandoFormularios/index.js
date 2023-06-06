37d06bb9226cbd8567d54e9875c146cd7e1de3b4
si390 <soyolaiberico@udesa.edu.ar>
2023-06-06 08:27:15 -0300

    validandoformularios

---
+++ Clase21 ValidandoFormularios/index.js
@@ -0,0 +1,32 @@
+console.log("Validando formularios");
+//Podemos capturar un campo usando clases o también con el atributo name.
+//focus
+let campoEmail = document.querySelector("[name=email]");
+campoEmail.addEventListener('focus', function() { 
+    //Nuestro código a ejecutar
+});
+
+//blur
+campoEmail.addEventListener('blur', function() {
+    //Nuestro dcódigo a ejecutar
+});
+
+//input
+campoEmail.addEventListener('input', function(){
+    //Nuestro código a ejecutar
+});
+
+//submit
+let formulario = document.querySelector("form");
+formulario.addEventListener('submit', function(event){
+    event.preventDefault(); //Detenemos el comportamiento default del formulario que es enviarse
+    if('campo vacío'){
+        //Informamos al usuario en el html
+    }
+});
+
+//value
+let campoBuscar = document.querySelector(".campoBuscar");
+if(campoBuscar.value==""){
+    //Informamos al usuario en el HTML
+}
